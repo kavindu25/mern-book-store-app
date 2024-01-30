6ed5dc323891888a421758284bcb9eada72df202
Kavindu <kavindudealwis@gmail.com>
2024-01-30 21:26:16 +0530

    feat: added react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
 
 const App = () => {
-  return (
-    <div className='text-black'>App</div>
-  )
-}
+  return(
+    <Routes>
+      <Route path="/" element={Home} />
+      <Route path="/books/create" element={CreateBook} />
+      <Route path="/books/details/:id" element={ShowBook} />
+      <Route path="/books/edit/:id" element={EditBook} />
+      <Route path="/books/delete/:id" element={DeleteBook} />
 
-export default App
+    </Routes>
+  );
+};
+
+export default App;
