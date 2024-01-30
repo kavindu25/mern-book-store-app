3ca6d45fea508735e88de8ab7fab242e07308194
Kavindu <kavindudealwis@gmail.com>
2024-01-30 10:42:33 +0530

    feat: added cors policy

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-//middleware for parsing request body
+// middleware for parsing request body
 app.use(express.json());
+
+// middleware for handling the cors policy
+// method 1 - allowing all origins with the default of cors(*)
+// app.use(cors());
+
+// method 2 - allow custom origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
