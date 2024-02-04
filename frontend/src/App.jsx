762fa4319c34fb885879c689a69d6b398bcb1f67
Kavindu <kavindudealwis@gmail.com>
2024-02-04 11:19:55 +0530

    feat: added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateBook from "./pages/CreateBook";
@@ -7,14 +7,13 @@ import EditBook from "./pages/EditBook";
 import DeleteBook from "./pages/DeleteBook";
 
 const App = () => {
-  return(
+  return (
     <Routes>
-      <Route path="/" element={Home} />
-      <Route path="/books/create" element={CreateBook} />
-      <Route path="/books/details/:id" element={ShowBook} />
-      <Route path="/books/edit/:id" element={EditBook} />
-      <Route path="/books/delete/:id" element={DeleteBook} />
-
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
 };
