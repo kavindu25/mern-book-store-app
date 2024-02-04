8390588cd7a208d533785b1758e7c4f181b055b9
Kavindu <kavindudealwis@gmail.com>
2024-02-04 11:21:13 +0530

    fix: cors policy fix

--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,16 @@ app.use(express.json());
 
 // middleware for handling the cors policy
 // method 1 - allowing all origins with the default of cors(*)
-// app.use(cors());
+app.use(cors());
 
 // method 2 - allow custom origins
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
