c41d644f53006fa4224245062c8c2be3e402e3b9
Kavindu <kavindudealwis@gmail.com>
2024-02-09 23:01:36 +0530

    feat: added bookcard component

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineDelete } from "react-icons/md";
 
 const BooksTable = ({books}) => {
   return (
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+// import { AiOutlineEdit } from "react-icons/ai";
+// import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox } from "react-icons/md";
 import BooksTable from "../components/home/BooksTable";
+import BooksCard from "../components/home/BooksCard";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [viewType, setViewType] = useState("table");
 
   useEffect(() => {
     setIsLoading(true);
@@ -27,13 +29,37 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-300 hover:bg-sky-400 px-4 py-1 rounded-lg"
+          onClick={() => {
+            setViewType("table");
+          }}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-300 hover:bg-sky-400 px-4 py-1 rounded-lg"
+          onClick={() => {
+            setViewType("card");
+          }}
+        >
+          Card
+        </button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
-      {isLoading ? <Spinner /> : <BooksTable books={books} />}
+      {isLoading ? (
+        <Spinner />
+      ) : viewType === "table" ? (
+        <BooksTable books={books} />
+      ) : (
+        <BooksCard books={books} />
+      )}
     </div>
   );
 };
